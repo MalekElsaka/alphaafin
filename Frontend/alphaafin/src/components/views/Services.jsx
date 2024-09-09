286ceca72a413f24769e863a745dc6025788b652
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2024-09-10 02:54:12 +0300

    Modify services width

--- Frontend/alphaafin/src/components/views/Services.jsx
+++ Frontend/alphaafin/src/components/views/Services.jsx
@@ -63,19 +63,21 @@ const SvgComponent = () => (
 const Services = () => {
     return (
         <>
-            <div className="flex flex-row max-w-[1260px] bg-[#F5F5F5] mx-auto mb-2 mt-2 w-full">
-                <p className="font-cairo text-[46px] font-semibold">Services</p>
-            </div>
+            <div className="bg-[#F5F5F5] w-full">
+                <div className="flex flex-row max-w-[1260px] mx-auto mb-2 mt-2 w-full">
+                    <p className="font-cairo text-[46px] font-semibold">Services</p>
+                </div>
 
-            <div className="relative w-full h-[350px] flex flex-col items-center justify-center"
-                style={{ backgroundImage: `url(${"https://alphaafin.com.au/wp-content/uploads/2024/08/img-section-12-1.jpg"})`, backgroundSize: "cover", backgroundPosition: "center 50%" }}>
-                <div className="absolute inset-0 bg-alpha_gray opacity-70"></div>
+                <div className="relative w-full h-[350px] flex flex-col items-center justify-center"
+                    style={{ backgroundImage: `url(${"https://alphaafin.com.au/wp-content/uploads/2024/08/img-section-12-1.jpg"})`, backgroundSize: "cover", backgroundPosition: "center 50%" }}>
+                    <div className="absolute inset-0 bg-alpha_gray opacity-70"></div>
 
-                <div className='z-20 mt-16 w-full h-full flex flex-col items-center justify-center'>
-                    <p className='text-white text-[46px] font-cairo font-semibold'>Services</p>
+                    <div className='z-20 mt-16 w-full h-full flex flex-col items-center justify-center'>
+                        <p className='text-white text-[46px] font-cairo font-semibold'>Services</p>
 
-                    <div className='w-[120px] -mt-4 h-[120px]'>
-                        <SvgComponent />
+                        <div className='w-[120px] -mt-4 h-[120px]'>
+                            <SvgComponent />
+                        </div>
                     </div>
                 </div>
             </div>
