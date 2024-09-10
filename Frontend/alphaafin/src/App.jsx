460eaa5806f8e15919479494eae98e9d2d477ff0
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2024-09-10 03:01:17 +0300

    Refactor code structure and lazy load components in App.jsx

--- Frontend/alphaafin/src/App.jsx
+++ Frontend/alphaafin/src/App.jsx
@@ -1,18 +1,24 @@
-import { useState, lazy, Suspense } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+const Home = lazy(() => import('./components/views/Home'));
+const Navbar = lazy(() => import('./components/navbar/Navbar'));
+const AboutUs = lazy(() => import('./components/views/AboutUs'));
+const Faqs = lazy(() => import('./components/views/Faqs'));
+const Services = lazy(() => import('./components/views/Services'));
+
+
+const Loading = () => <div>Loading...</div>;
 
 function App() {
-
-  const Home = lazy(() => import('./components/views/Home'))
-  const Navbar = lazy(()=>import('./components/navbar/Navbar'))
-  const AboutUs = lazy(()=>import('./components/views/AboutUs'))
-  const Faqs =  lazy(()=>import('./components/views/Faqs'))
-  const Services = lazy(()=>import('./components/views/Services'))
   return (
-    <Router basename='/alphaafin'>
-      <div className='w-full h-full bg-white flex flex-col'>
-        <Suspense fallback={<div>Loading...</div>}>
+    <Router basename="/alphaafin">
+      <div className="w-full h-full bg-white flex flex-col">
+        <Suspense fallback={<Loading />}>
           <Navbar />
+        </Suspense>
+        <Suspense fallback={<Loading />}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about-us" element={<AboutUs />} />
@@ -22,7 +28,7 @@ function App() {
         </Suspense>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
