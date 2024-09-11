432cc99a619eecd16285a6b727680ab2432ce60c
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2024-09-12 01:32:53 +0300

    Remove navbar and home from lazy loading

--- Frontend/alphaafin/src/App.jsx
+++ Frontend/alphaafin/src/App.jsx
@@ -1,13 +1,11 @@
-import { lazy, Suspense } from 'react';
+import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
-const Home = lazy(() => import('./components/views/Home'));
-const Navbar = lazy(() => import('./components/navbar/Navbar'));
+import Home from './components/views/Home';
+import Navbar from './components/navbar/Navbar';
 const AboutUs = lazy(() => import('./components/views/AboutUs'));
 const Faqs = lazy(() => import('./components/views/Faqs'));
 const Services = lazy(() => import('./components/views/Services'));
-
 
 const Loading = () => <div>Loading...</div>;
 
@@ -15,9 +13,7 @@ function App() {
   return (
     <Router basename="/alphaafin">
       <div className="w-full h-full bg-white flex flex-col">
-        <Suspense fallback={<Loading />}>
-          <Navbar />
-        </Suspense>
+        <Navbar />
         <Suspense fallback={<Loading />}>
           <Routes>
             <Route path="/" element={<Home />} />
